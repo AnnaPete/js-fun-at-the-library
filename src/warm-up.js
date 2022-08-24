2abf25749235058e9cb7adb667440f66a1f3da66
Anna Peterson (she/her) <107816053+AnnaPete@users.noreply.github.com>
2022-08-24 16:07:20 -0600

    Update warm-up.js

--- src/warm-up.js
+++ src/warm-up.js
@@ -13,27 +13,30 @@
 // var title = "Harry Potter";
 // var ...
 
+var title = "The Hobbit";
+var pages = 306;
 
 
 // Reassign (not initialize) your "pages" variable from above. An editor came around and
 // condensed the book, use a built in math operator to make the page count a quarter
 // of what it currently is 
 
-
+pages /= 4;
 
 // Initialize a new variable "movie", but leave it undefined
 
+var movie
 
 
 // Create a variable named "sequel", and assign it to a be a sequel of your
 // book (made up or not!). Your sequel must make use of your "title" variable from above
 
-
+var sequel = title + ": The One With The Dragon";
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the book you chose is your favorite book or not
 
-
+var isFavorite = true;
 
 
 // Create a new variable named "isShortStory", and use a comparison operator to
@@ -41,8 +44,14 @@
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
 
-
-
+var isShortStory = 
+if (pages < 25) {
+  isShortStory === true
+} else {
+  isShortStory === false
+}
+  
+  
 //------------------------- Conditionals -------------------------//
 
 
